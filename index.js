cd232ef7aa622b0b6cc63fb7b00c300dde540c45
Azrael Rex Anacleto <anacletoazraelrex@gmail.com>
2024-03-29 01:41:35 +0800

    Changed third line
    Removed a word in the third line of Index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // console.log("Hi Man")
         document.getElementById('btn').addEventListener('click' , function(){
-        document.getElementById('BTS').innerHTML = 'Hello welcome to my Javascript website'
+        document.getElementById('BTS').innerHTML = 'Hello welcome to my website'
     })
  
 const obj =[{ name: "John", age: 30, city: "New York"}, 
